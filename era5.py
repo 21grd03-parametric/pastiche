a5fe70078aea0dce6296115a13fda3fc94696386
Claudio Belotti <claudio.belotti@cnr.it>
2025-07-04 17:54:13 +0200

    added era5 and planck

---
+++ era5.py
@@ -0,0 +1,85 @@
+import numpy as np
+import datetime
+import logging
+# from sklearn.neighbors import BallTree
+
+def closest_gridpoint(x, latitude=None, longitude=None):
+    '''
+    closest_gridpoint(x, latitude=None, longitude=None):
+    given x 
+    x = (lat, lon)
+    finds c closest ERA5 gridpoint to x.
+    returns:
+    indices (i,j) of the point wrt the grid
+    c=(lat,lon);
+    if latitude and longitude are None (default) 
+    assumes a full ERA5 grid 721x1440, 0.25 deg resolution;
+    distance from x to c in km
+    '''
+    if latitude is None:
+        latitude = np.linspace(90., -90., 721) # full ERA5 grid
+    if longitude is None:
+        longitude = np.linspace(0., 359.75, 1440) # full ERA5 grid
+
+    LT,LN = np.meshgrid(latitude,longitude)
+    D = __haversine((LT.ravel(),LN.ravel()), x)
+    ii = D.argmin()
+    d = D[ii]
+    indices = np.unravel_index(ii, LT.shape) # (ln,lt)
+    indices = (indices[1], indices[0]) # swap -> (lt,ln)
+    c = (latitude[indices[0]], longitude[indices[1]])
+
+    # if lon of x is negative
+    # adapt c lon to -180:180 grid
+    if (x[1]<0) and (c[1]>=180):
+        c = (latitude[indices[0]], longitude[indices[1]]-360)
+
+    logging.debug(f"x={x}, c={c}, indices={indices}, d(x,c)={d:.3} km")
+    return indices, c, d
+    
+def __haversine(A, B):
+    """
+    Calculate the great circle distance in kilometers between two points 
+    on the earth (specified in decimal degrees)
+
+    see: https://stackoverflow.com/a/4913653
+    """
+    Alt, Aln = A
+    Blt, Bln = B
+    # convert decimal degrees to radians 
+    lat1, lon1, lat2, lon2 = map(np.deg2rad, [Alt, Aln, Blt, Bln])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
+    c = 2 * np.arcsin(np.sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+    return c * r
+
+def z2h(z, Re=6371.2290, g0=9.80665):
+    ''' geopotential [m**2 s**-2] to altitude [m]
+    Ref:
+    https://confluence.ecmwf.int/display/CKB/ERA5%3A+compute+pressure+and+geopotential+on+model+levels%2C+geopotential+height+and+geometric+height#heading-Geometricheight
+    https://unidata.github.io/MetPy/latest/api/generated/metpy.calc.geopotential_to_height.html#metpy.calc.geopotential_to_height
+    '''
+    Re *= 1000 # m
+    return z*Re/(g0*Re-z)
+
+
+def time2datetime(e5time):
+    d0 = datetime.datetime(1900, 1, 1, 0, 0, 0)
+    if hasattr(e5time, '__len__'):
+        d1 = [d0 + datetime.timedelta(hours=int(t)) for t in e5time.flatten()]
+        if len(d1)==1: # this is UGLY
+            d1 = d1[0]
+    else:
+        d1 = d0 + datetime.timedelta(hours=int(e5time))
+    return d1
+
+
+def np_time2datetime(e5time):
+    d0 = np.datetime64('1900-01-01')
+    d1 = d0 + e5time.astype('timedelta64[h]')
+    return d1
+
---
+++ planck.py
@@ -0,0 +1,174 @@
+import numpy as np
+
+# see
+# https://www.spectralcalc.com/blackbody/CalculatingBlackbodyRadianceV2.pdf
+
+K = 1.3806488e-23 # Boltzmann constant [JK^-1]
+h = 6.62606957e-34 # Planck constant [Js]
+c = 2.99792458e8 # speed of light ms^-1
+c1 = 2e8*h*c**2
+c2 = 100*h*c/K
+
+def B(sigma, T):
+    '''
+    B(sigma,T):
+    Blackbody spectral radiance per wavenumber [cm-1]
+    [W (m2 sr cm-1)-1]
+    sigma: wavenumber [cm-1]
+    T: temperature [K]
+    '''
+    ## come B_sigma ma lo lascio per ragioni "storiche"
+    s = sigma
+    return c1*s**3/(np.exp(c2*s/T)-1) # W (m^2 sr cm^-1)^-1
+
+
+def B_sigma(sigma, T):
+    '''
+    B_sigma(sigma,T):
+    Blackbody spectral radiance per wavenumber [cm-1]
+    [W (m2 sr cm-1)-1]
+    sigma: wavenumber [cm-1]
+    T: temperature [K]
+    '''
+    v = 100*c*sigma
+    return 100*c*B_nu(v,T) # W (m^2 sr cm^-1)^-1
+
+
+def B_nu(nu, T):
+    '''
+    B(nu,T):
+    Blackbody spectral radiance per frequency [Hz]
+    [W (m2 sr Hz)-1]
+    nu: frequency [Hz]
+    T: temperature [K]
+    '''
+    a = 2*h/c**2
+    b = h/K
+    return a*nu**3/(np.exp(b*nu/T)-1) # W (m^2 sr Hz)^-1
+
+
+def sigma_peak(T):
+    '''
+    sigma_peak(T) [cm-1]
+    wavenumber of the maximum spectral radiance
+
+    Notice that for a given temperature, parameterization by frequency (or wavenumber) 
+    implies a different maximal wavelength than parameterization by wavelength.
+    https://en.wikipedia.org/wiki/Wien%27s_displacement_law#Maxima_differ_according_to_parameterization    '''
+    return  nu_peak(T)/(100*c)# [cm-1]
+
+
+def nu_peak(T):
+    '''
+    nu_peak(T) [Hz]
+    frequency of the maximum spectral radiance
+
+    Notice that for a given temperature, parameterization by frequency (or wavenumber) 
+    implies a different maximal wavelength than parameterization by wavelength.
+    https://en.wikipedia.org/wiki/Wien%27s_displacement_law#Maxima_differ_according_to_parameterization    '''
+    a3 = 2.82143937212
+    return a3*K*T/h # [Hz]
+
+
+def dBdT(sigma, T):
+    '''
+    dBdT(sigma, T)
+    sigma: wavenumber [cm-1]
+    T: temperature [K]
+    '''
+    s = sigma
+    return c1*s**3/(np.exp(c2*s/T)-1)**2 * np.exp(c2*s/T) * (c2*s/T**2)
+
+
+def BT(sigma, L, epsilon=1.0):
+    '''
+    BT(sigma,L,epsilon=1.0):
+    Brightness temperature per wavenumber [cm-1]
+    [K]
+    sigma: wavenumber [cm-1]
+    L: radiance [W (m2 sr cm-1)-1]
+    epsilon: emissivity
+    '''
+    s = sigma
+    return c2*s/np.log(1 + epsilon*c1*(s**3)/L)
+
+
+def B_integral_band(sigma1, sigma2, temperature):
+    # integral of spectral radiance over a spectral band
+    # from sigma1 to sigma2 (cm-1)
+    # returns [W m-2 sr-1] 
+    return __planck_integral(sigma1, temperature)- __planck_integral(sigma2, temperature)
+
+
+def __planck_integral(sigma, temperature):
+    # https://www.spectralcalc.com/blackbody/appendixA.html 
+    # integral of spectral radiance from sigma (cm-1) to infinity.
+    # result is W/m2/sr.
+    # follows Widger and Woodall, Bulletin of the American Meteorological
+    # Society, Vol. 57, No. 10, pp. 1217
+ 
+    # constants
+
+    Planck =  6.6260693e-34     
+    Boltzmann = 1.380658e-23 
+    Speed_of_light = 299792458.0 
+    Speed_of_light_sq = Speed_of_light * Speed_of_light 
+
+    # compute powers of x, the dimensionless spectral coordinate
+
+    c1 =  (Planck*Speed_of_light/Boltzmann) 
+    x =  c1 * 100 * sigma / temperature 
+    x2 = x *  x  
+    x3 = x *  x2 
+
+    # decide how many terms of sum are needed
+
+    iterations = 2.0 + 20.0/x ;
+    iterations = iterations if (iterations<512) else 512
+    iter = int(iterations)
+
+    # add up terms of sum
+
+    sum = 0  ;
+    for n in range(1,iter):
+        dn = 1.0/n ;
+        sum  += np.exp(-n*x)*(x3 + (3.0 * x2 + 6.0*(x+dn)*dn)*dn)*dn
+
+    # return result, in units of W/m2/sr
+    c2 =  (2.0*Planck*Speed_of_light_sq)
+    return  sum*c2*(temperature/c1)**4
+
+
+def B_lambda(l,T):
+    '''
+    B_lambda(l,T):
+    Blackbody spectral radiance per wavelength [µm]
+    [W m-2 sr-1 µm-1]
+    l: wavelength [µm]
+    T: temperature K
+    '''
+    
+    a = 2e24*h*c**2
+    b = 1e6*h*c/K
+    return a/l**5/(np.exp(b/(l*T))-1)
+
+
+def lambda_peak(T):
+    '''
+    lmabda_peak(T) [µm]
+    wavelength of the maximum spectral radiance
+
+    Notice that for a given temperature, parameterization by frequency (or wavenumber) 
+    implies a different maximal wavelength than parameterization by wavelength.
+    https://en.wikipedia.org/wiki/Wien%27s_displacement_law#Maxima_differ_according_to_parameterization    '''
+    a5 = 4.96511423174    
+    return 1e6*h*c/(a5*K*T) # [µm]
+
+
+def stefan_boltzmann(T):
+    sigma = 5.670367e-8 # W m**-2 K**-4
+    return sigma*np.power(T,4)
+
+def inv_stefan_boltzmann(F):
+    sigma = 5.670367e-8 # W m**-2 K**-4
+    return np.power(F/sigma,1./4)
